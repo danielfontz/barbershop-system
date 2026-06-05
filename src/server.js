eb05ec215b1a30fc601842de34446ced851776c1
danielfontz <danielfontz056@gmail.com>
2026-06-05 16:00:15 -0300

    fix: correct cors import and formatting in server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 import express from 'express';
+import cors from 'cors';
 import authRoutes from './routes/auth.routes.js';
 import barberRoutes from './routes/barber.routes.js';
 import schedulingRoutes from './routes/scheduling.routes.js';
 import serviceRoutes from './routes/service.routes.js';
 
 const prisma = new PrismaClient();
-
 const app = express();
-app.use(express.json());
 
-const cors = require('cors');
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Bem-vindo à API de agendamento de barbearia!');
 });
 
+// 4. ROTAS DA APLICAÇÃO
 app.use('/api/auth', authRoutes);
 app.use('/api/barber', barberRoutes);
 app.use('/api/schedules', schedulingRoutes);
